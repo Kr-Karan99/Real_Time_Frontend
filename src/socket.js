568dcd72490de9056ab269b803bb27f01d851e02
Karan Kumar <myselfkaran124@gmail.com>
2025-06-29 16:32:43 +0530

    first commit

---
+++ src/socket.js
@@ -0,0 +1,12 @@
+import { io } from 'socket.io-client';
+
+export const initSocket = async () => {
+    const options = {
+        'force new connection': true,
+        reconnectionAttempts: Infinity, // fix typo
+        timeout: 10000,
+        transports: ['websocket'],
+    };
+    // Use your backend's WebSocket URL here
+    return io('https://realtimebackend-production.up.railway.app/', options);
+};
